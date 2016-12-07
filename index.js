670dfb9ce0bddb6f379a081967d8ab1793ef8dfb
Jamieson Bates <jamiesonbates@gmail.com>
2016-12-07 10:17:27 -0800

    Close #7 Build functionality that allows user to add quotes to their quiz and result

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 //
 // -----------------------------------------------------------------------------
   let statementsObjSet;
+  let allStatements = [];
+  let dataSet;
 
   const getStatements = function() {
     const $xhr = $.ajax({
@@ -19,145 +21,157 @@
       if ($xhr.status !== 200) {
         return;
       }
-
-      const parties = ['Republican', 'Democrat', 'Independent', 'Libertarian', 'Green'];
-
-      const statementsInclusive = data.objects.map((statement) => {
-
-        if (parties.includes(statement.speaker.party.party) && statement.speaker.first_name !== '') {
-          const statementObj = {
-            quote: (statement.statement).trim(),
-            date: statement.statement_date,
-            statementContext: statement.statement_context,
-            statementUrl: `http://www.politifact.com${statement.canonical_url}`,
-            ruling: statement.ruling.ruling,
-            rulingGraphic: statement.ruling.ruling_graphic,
-            rulingSummary: statement.ruling_headline,
-            speaker: {
-              name: `${statement.speaker.first_name} ${statement.speaker.last_name}`,
-              position: statement.speaker.current_job,
-              state: statement.speaker.home_state,
-              party: statement.speaker.party.party,
-              photoUrl: statement.speaker.photo
-            }
-          };
-
-          return statementObj;
-        }
-      });
-
-      const statements = statementsInclusive.filter((statement) => {
-        return statement;
-      });
-
-      const statementsIndex = [];
-      const statementsLength = statements.length;
-
-      while (statementsIndex.length < 10) {
-        const randomNumber = Math.floor(Math.random() * statementsLength);
-
-        if (!statementsIndex.includes(randomNumber)) {
-          statementsIndex.push(randomNumber);
-        }
-      }
-
-      const statementsObjTempSet = statements.filter((statement, index) => {
-        if (statementsIndex.includes(index)) {
-          return statement;
-        }
-      });
-
-      const allSpeakers = statements.map((statement) => {
-        return statement.speaker; // .name
-      });
-
-      const answerSet = function(speaker, speakersGroup) {
-        const unrandomized = [speaker];
-        const randomizedIndex = [];
-        const randomized = [];
-        const taken = [];
-
-        while (unrandomized.length < 4) {
-          const randomI = Math.floor(Math.random() * speakersGroup.length);
-          const proposed = speakersGroup[randomI];
-          let takenBool = false;
-
-          for (const speakerObj of taken) {
-            if (speakerObj.name === proposed.name) {
-              takenBool = true;
-            }
-          }
-
-          if (proposed.name !== speaker.name && !takenBool) {
-            unrandomized.push(proposed);
-            taken.push(proposed);
-          }
-        }
-
-        while (randomizedIndex.length < 4) {
-          const randomSpeaker = Math.floor(Math.random() * unrandomized.length);
-
-          if (!randomizedIndex.includes(randomSpeaker)) {
-            randomizedIndex.push(randomSpeaker);
-          }
-        }
-
-        for (let i = 0; i < 4; i++) {
-          randomized[i] = unrandomized[randomizedIndex[i]];
-        }
-
-        return randomized;
-      };
-
-      statementsObjSet = statementsObjTempSet.map((statementsObj) => {
-        statementsObj.answerSet = answerSet(statementsObj.speaker, allSpeakers);
-
-        let editedQuote = statementsObj.quote;
-
-        editedQuote = editedQuote.replace('<p>', '');
-        editedQuote = editedQuote.replace('</p>', '');
-        editedQuote = editedQuote.replace('<div>', '');
-        editedQuote = editedQuote.replace('<div>', '');
-        editedQuote = editedQuote.replace('</div>', '');
-        editedQuote = editedQuote.replace(/(&quot;)/g, '"');
-        editedQuote = editedQuote.replace(/(&rsquo;)/g, "'");
-        editedQuote = editedQuote.replace(/(&#39;)/g, "'");
-        editedQuote = editedQuote.replace(/(<div dir="ltr">)/g, '');
-        editedQuote = editedQuote.replace(/(&nbsp;<\/div>)/g, '');
-        editedQuote = editedQuote.replace(/(<p> <\/p>)/g, '');
-        editedQuote = editedQuote.replace(/(<p>&nbsp;<\/p>)/g, '');
-        editedQuote = editedQuote.replace('&nbsp;', ' ');
-        editedQuote = editedQuote.replace('&hellip;', '...');
-        editedQuote = editedQuote.replace('&amp;', '&');
-        editedQuote = editedQuote.replace('<br />', ' ');
-
-        statementsObj.quote = `${editedQuote}`;
-
-        return statementsObj;
-      });
-
-      // populate page with first quote
-      $('#quote-text').text(statementsObjSet[0].quote);
-      $('#context').text(`Said in ${statementsObjSet[0].statementContext}`);
-      $('#answer-1').text(statementsObjSet[0].answerSet[0].name);
-      $('#answer-2').text(statementsObjSet[0].answerSet[1].name);
-      $('#answer-3').text(statementsObjSet[0].answerSet[2].name);
-      $('#answer-4').text(statementsObjSet[0].answerSet[3].name);
+      dataSet = data;
+      organizeData();
     });
   };
 
   getStatements();
+
+  const organizeData = function() {
+
+    const parties = ['Republican', 'Democrat', 'Independent', 'Libertarian', 'Green'];
+
+    const statementsInclusive = dataSet.objects.map((statement) => {
+
+      if (parties.includes(statement.speaker.party.party) && statement.speaker.first_name !== '' && !allStatements.includes(`http://www.politifact.com${statement.canonical_url}`)) {
+        const statementObj = {
+          quote: (statement.statement).trim(),
+          date: statement.statement_date,
+          statementContext: statement.statement_context,
+          statementUrl: `http://www.politifact.com${statement.canonical_url}`,
+          ruling: statement.ruling.ruling,
+          rulingGraphic: statement.ruling.ruling_graphic,
+          rulingSummary: statement.ruling_headline,
+          speaker: {
+            name: `${statement.speaker.first_name} ${statement.speaker.last_name}`,
+            position: statement.speaker.current_job,
+            state: statement.speaker.home_state,
+            party: statement.speaker.party.party,
+            photoUrl: statement.speaker.photo
+          }
+        };
+
+        return statementObj;
+      }
+    });
+
+    const statements = statementsInclusive.filter((statement) => {
+      return statement;
+    });
+
+    const statementsIndex = [];
+    const statementsLength = statements.length;
+
+    while (statementsIndex.length < 10) {
+      const randomNumber = Math.floor(Math.random() * statementsLength);
+
+      if (!statementsIndex.includes(randomNumber)) {
+        statementsIndex.push(randomNumber);
+      }
+    }
+
+    const statementsObjTempSet = statements.filter((statement, index) => {
+      if (statementsIndex.includes(index)) {
+        return statement;
+      }
+    });
+
+    const allSpeakers = statements.map((statement) => {
+      return statement.speaker; // .name
+    });
+
+    const answerSet = function(speaker, speakersGroup) {
+      const unrandomized = [speaker];
+      const randomizedIndex = [];
+      const randomized = [];
+      const taken = [];
+
+      while (unrandomized.length < 4) {
+        const randomI = Math.floor(Math.random() * speakersGroup.length);
+        const proposed = speakersGroup[randomI];
+        let takenBool = false;
+
+        for (const speakerObj of taken) {
+          if (speakerObj.name === proposed.name) {
+            takenBool = true;
+          }
+        }
+
+        if (proposed.name !== speaker.name && !takenBool) {
+          unrandomized.push(proposed);
+          taken.push(proposed);
+        }
+      }
+
+      while (randomizedIndex.length < 4) {
+        const randomSpeaker = Math.floor(Math.random() * unrandomized.length);
+
+        if (!randomizedIndex.includes(randomSpeaker)) {
+          randomizedIndex.push(randomSpeaker);
+        }
+      }
+
+      for (let i = 0; i < 4; i++) {
+        randomized[i] = unrandomized[randomizedIndex[i]];
+      }
+
+      return randomized;
+    };
+
+    statementsObjSet = statementsObjTempSet.map((statementsObj) => {
+      statementsObj.answerSet = answerSet(statementsObj.speaker, allSpeakers);
+
+      let editedQuote = statementsObj.quote;
+
+      editedQuote = editedQuote.replace('<p>', '');
+      editedQuote = editedQuote.replace('</p>', '');
+      editedQuote = editedQuote.replace('<div>', '');
+      editedQuote = editedQuote.replace('<div>', '');
+      editedQuote = editedQuote.replace('</div>', '');
+      editedQuote = editedQuote.replace(/(&quot;)/g, '"');
+      editedQuote = editedQuote.replace(/(&rsquo;)/g, "'");
+      editedQuote = editedQuote.replace(/(&#39;)/g, "'");
+      editedQuote = editedQuote.replace(/(<div dir="ltr">)/g, '');
+      editedQuote = editedQuote.replace(/(&nbsp;<\/div>)/g, '');
+      editedQuote = editedQuote.replace(/(<p> <\/p>)/g, '');
+      editedQuote = editedQuote.replace(/(<p>&nbsp;<\/p>)/g, '');
+      editedQuote = editedQuote.replace('&nbsp;', ' ');
+      editedQuote = editedQuote.replace('&hellip;', '...');
+      editedQuote = editedQuote.replace('&amp;', '&');
+      editedQuote = editedQuote.replace('<br />', ' ');
+
+      statementsObj.quote = `${editedQuote}`;
+
+      return statementsObj;
+    });
+
+    // populate page with first quote
+    $('#quote-text').text(statementsObjSet[0].quote);
+    $('#context').text(`Said in ${statementsObjSet[0].statementContext}`);
+    $('#answer-1').text(statementsObjSet[0].answerSet[0].name);
+    $('#answer-2').text(statementsObjSet[0].answerSet[1].name);
+    $('#answer-3').text(statementsObjSet[0].answerSet[2].name);
+    $('#answer-4').text(statementsObjSet[0].answerSet[3].name);
+
+    for (const statement of statementsObjSet) {
+      allStatements.push(statement.statementUrl);
+    }
+    // console.log(statementsObjSet);
+    // console.log(allStatements);
+  }
 
 // -----------------------------------------------------------------------------
 //
 //  Render Data on Page and Add Functionality For Quiz
 //
 // -----------------------------------------------------------------------------
-  let quoteCount = 0;
+  let quoteCount = 1;
+  let setCount = 0;
 
   const partOne = function($target) {
     const answer = $target.text();
-    const statement = statementsObjSet[quoteCount];
+    const statement = statementsObjSet[setCount];
 
     statement.speakerGuess = answer;
 
@@ -192,32 +206,36 @@
   const partTwo = function($target) {
     const answer = $target.text();
 
-    statementsObjSet[quoteCount].truthGuess = answer;
+    statementsObjSet[setCount].truthGuess = answer;
 
-    if (statementsObjSet[quoteCount].truthGuess === statementsObjSet[quoteCount].ruling) {
-      $('#ruling').text(`Correct! The statement is ${statementsObjSet[quoteCount].ruling}`);
+    if (statementsObjSet[setCount].truthGuess === statementsObjSet[setCount].ruling) {
+      $('#ruling').text(`Correct! The statement is ${statementsObjSet[setCount].ruling}`);
     }
 
-    if (statementsObjSet[quoteCount].truthGuess !== statementsObjSet[quoteCount].ruling) {
-      $('#ruling').text(`Incorrect! The statement is actually ${statementsObjSet[quoteCount].ruling}`);
+    if (statementsObjSet[setCount].truthGuess !== statementsObjSet[setCount].ruling) {
+      $('#ruling').text(`Incorrect! The statement is actually ${statementsObjSet[setCount].ruling}`);
     }
 
     $('#prompt h2').text('');
 
     $('#truth-photo').removeClass('off');
-    $('#truth-photo').attr('src', statementsObjSet[quoteCount].rulingGraphic);
+    $('#truth-photo').attr('src', statementsObjSet[setCount].rulingGraphic);
     $('#summary-header').removeClass('off');
-    $('#ruling-summary').text(statementsObjSet[quoteCount].rulingSummary);
+    $('#ruling-summary').text(statementsObjSet[setCount].rulingSummary);
     $('#source').removeClass('off');
-    $('#source').attr('href', statementsObjSet[quoteCount].statementUrl);
+    $('#source').attr('href', statementsObjSet[setCount].statementUrl);
 
     $('.tf-choices').toggleClass('off');
 
     $('#submit').removeClass('off');
+
+    if (setCount === 9) {
+      $('#add-quotes').removeClass('off');
+    }
   };
 
   const nextQuestion = function() {
-    const statement = statementsObjSet[quoteCount];
+    const statement = statementsObjSet[setCount];
 
     $('#quote-text').text(statement.quote);
     $('#answer-1').text(statement.answerSet[0].name);
@@ -228,7 +246,7 @@
     $('#photo').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/1024px-Placeholder_no_text.svg.png');
     $('#result').text('');
     $('#ruling').text('');
-    $('#quote-count').text(`Quote ${quoteCount + 1} of 10`);
+    $('#quote-count').text(`Quote ${quoteCount} of ${allStatements.length}`);
     $('#submit').addClass('off');
     $('#prompt h2').text('Can you guess which politician said this?');
     $('#context').text(`Said in ${statement.statementContext}.`);
@@ -239,10 +257,15 @@
     $('#position').text('');
     $('#party').text('');
     $('#state').text('');
+
+    stepOneComplete = false;
+    stepTwoComplete = false;
+    $('.p-choices').toggleClass('off');
   };
 
   const buildResults = function() {
     let countQuotes = 1;
+
     for (const statement of statementsObjSet) {
       const $ul = $('.results-list');
       const $li = $('<li>');
@@ -286,15 +309,16 @@
     else if (stepOneComplete && !stepTwoComplete) {
       partTwo($target);
       stepTwoComplete = true;
+      setCount += 1;
       quoteCount += 1;
     }
-    else if (stepOneComplete && stepTwoComplete && quoteCount < 10) {
+    else if (stepOneComplete && stepTwoComplete && setCount < 10) {
       nextQuestion();
-      stepOneComplete = false;
-      stepTwoComplete = false;
-      $('.p-choices').toggleClass('off');
+      if (setCount === 9) {
+        $('#submit').text('See Results');
+      }
     }
-    else if (quoteCount === 10) {
+    else if (setCount === 10) {
       $('#quiz').addClass('off');
       $('#results').removeClass('off');
       buildResults();
@@ -303,7 +327,7 @@
 
   $('.more-info').on('click', (event) => {
     const $target = $(event.target);
-    const stmt = statementsObjSet[quoteCount];
+    const stmt = statementsObjSet[setCount];
 
     if ($target.hasClass('answer-1')) {
       $('div .answer-1 .panel-photo').attr('src', stmt.answerSet[0].photoUrl);
@@ -336,5 +360,12 @@
       $('div .answer-4 .info-state').text('State: ' + stmt.answerSet[3].state);
       $('div.politician-panel.answer-4').toggleClass('off');
     }
+  });
+
+  $('#add-quotes').on('click', (event) => {
+    organizeData();
+    setCount = 0;
+    nextQuestion();
+    $('#add-quotes').addClass('off');
   });
 })();
