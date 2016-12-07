ad9f886076a6efdd37cb21b50378236da454ea0b
Jamieson Bates <jamiesonbates@gmail.com>
2016-12-07 10:38:04 -0800

    Close #27 Re-evaluate Data Model and Update

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
 //
 // -----------------------------------------------------------------------------
   let statementsObjSet;
-  let allStatements = [];
+  let allStatementsData = [];
+  let allStatementsUrl = [];
   let dataSet;
 
   const getStatements = function() {
@@ -34,21 +35,27 @@
 
     const statementsInclusive = dataSet.objects.map((statement) => {
 
-      if (parties.includes(statement.speaker.party.party) && statement.speaker.first_name !== '' && !allStatements.includes(`http://www.politifact.com${statement.canonical_url}`)) {
+      if (parties.includes(statement.speaker.party.party) && statement.speaker.first_name !== '' && !allStatementsUrl.includes(`http://www.politifact.com${statement.canonical_url}`)) {
         const statementObj = {
-          quote: (statement.statement).trim(),
-          date: statement.statement_date,
-          statementContext: statement.statement_context,
-          statementUrl: `http://www.politifact.com${statement.canonical_url}`,
-          ruling: statement.ruling.ruling,
-          rulingGraphic: statement.ruling.ruling_graphic,
-          rulingSummary: statement.ruling_headline,
+          statement: {
+            quote: (statement.statement).trim(),
+            date: statement.statement_date,
+            statementContext: statement.statement_context,
+            statementUrl: `http://www.politifact.com${statement.canonical_url}`
+          },
+          ruling: {
+            ruling: statement.ruling.ruling,
+            rulingGraphic: statement.ruling.ruling_graphic,
+            rulingSummary: statement.ruling_headline
+          },
           speaker: {
             name: `${statement.speaker.first_name} ${statement.speaker.last_name}`,
             position: statement.speaker.current_job,
             state: statement.speaker.home_state,
             party: statement.speaker.party.party,
             photoUrl: statement.speaker.photo
+          },
+          user: {
           }
         };
 
@@ -78,7 +85,7 @@
     });
 
     const allSpeakers = statements.map((statement) => {
-      return statement.speaker; // .name
+      return statement.speaker;
     });
 
     const answerSet = function(speaker, speakersGroup) {
@@ -122,7 +129,7 @@
     statementsObjSet = statementsObjTempSet.map((statementsObj) => {
       statementsObj.answerSet = answerSet(statementsObj.speaker, allSpeakers);
 
-      let editedQuote = statementsObj.quote;
+      let editedQuote = statementsObj.statement.quote;
 
       editedQuote = editedQuote.replace('<p>', '');
       editedQuote = editedQuote.replace('</p>', '');
@@ -141,24 +148,23 @@
       editedQuote = editedQuote.replace('&amp;', '&');
       editedQuote = editedQuote.replace('<br />', ' ');
 
-      statementsObj.quote = `${editedQuote}`;
+      statementsObj.statement.quote = `${editedQuote}`;
 
       return statementsObj;
     });
 
     // populate page with first quote
-    $('#quote-text').text(statementsObjSet[0].quote);
-    $('#context').text(`Said in ${statementsObjSet[0].statementContext}`);
+    $('#quote-text').text(statementsObjSet[0].statement.quote);
+    $('#context').text(`Said in ${statementsObjSet[0].statement.statementContext}`);
     $('#answer-1').text(statementsObjSet[0].answerSet[0].name);
     $('#answer-2').text(statementsObjSet[0].answerSet[1].name);
     $('#answer-3').text(statementsObjSet[0].answerSet[2].name);
     $('#answer-4').text(statementsObjSet[0].answerSet[3].name);
 
     for (const statement of statementsObjSet) {
-      allStatements.push(statement.statementUrl);
+      allStatementsUrl.push(statement.statement.statementUrl);
+      allStatementsData.push(statement);
     }
-    // console.log(statementsObjSet);
-    // console.log(allStatements);
   }
 
 // -----------------------------------------------------------------------------
@@ -173,13 +179,13 @@
     const answer = $target.text();
     const statement = statementsObjSet[setCount];
 
-    statement.speakerGuess = answer;
+    statement.user.speakerGuess = answer;
 
-    if (statement.speakerGuess === statement.speaker.name) {
+    if (statement.user.speakerGuess === statement.speaker.name) {
       $('#result').text(`Correct! ${statement.speaker.name} said this quote.`);
     }
 
-    if (statement.speakerGuess !== statement.speaker.name) {
+    if (statement.user.speakerGuess !== statement.speaker.name) {
       $('#result').text(`Incorrect! ${statement.speaker.name} said this quote.`);
     }
 
@@ -206,24 +212,24 @@
   const partTwo = function($target) {
     const answer = $target.text();
 
-    statementsObjSet[setCount].truthGuess = answer;
+    statementsObjSet[setCount].user.truthGuess = answer;
 
-    if (statementsObjSet[setCount].truthGuess === statementsObjSet[setCount].ruling) {
-      $('#ruling').text(`Correct! The statement is ${statementsObjSet[setCount].ruling}`);
+    if (statementsObjSet[setCount].user.truthGuess === statementsObjSet[setCount].ruling.ruling) {
+      $('#ruling').text(`Correct! The statement is ${statementsObjSet[setCount].ruling.ruling}`);
     }
 
-    if (statementsObjSet[setCount].truthGuess !== statementsObjSet[setCount].ruling) {
-      $('#ruling').text(`Incorrect! The statement is actually ${statementsObjSet[setCount].ruling}`);
+    if (statementsObjSet[setCount].user.truthGuess !== statementsObjSet[setCount].ruling.ruling) {
+      $('#ruling').text(`Incorrect! The statement is actually ${statementsObjSet[setCount].ruling.ruling}`);
     }
 
     $('#prompt h2').text('');
 
     $('#truth-photo').removeClass('off');
-    $('#truth-photo').attr('src', statementsObjSet[setCount].rulingGraphic);
+    $('#truth-photo').attr('src', statementsObjSet[setCount].ruling.rulingGraphic);
     $('#summary-header').removeClass('off');
-    $('#ruling-summary').text(statementsObjSet[setCount].rulingSummary);
+    $('#ruling-summary').text(statementsObjSet[setCount].ruling.rulingSummary);
     $('#source').removeClass('off');
-    $('#source').attr('href', statementsObjSet[setCount].statementUrl);
+    $('#source').attr('href', statementsObjSet[setCount].statement.statementUrl);
 
     $('.tf-choices').toggleClass('off');
 
@@ -237,7 +243,7 @@
   const nextQuestion = function() {
     const statement = statementsObjSet[setCount];
 
-    $('#quote-text').text(statement.quote);
+    $('#quote-text').text(statement.statement.quote);
     $('#answer-1').text(statement.answerSet[0].name);
     $('#answer-2').text(statement.answerSet[1].name);
     $('#answer-3').text(statement.answerSet[2].name);
@@ -246,10 +252,10 @@
     $('#photo').attr('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Placeholder_no_text.svg/1024px-Placeholder_no_text.svg.png');
     $('#result').text('');
     $('#ruling').text('');
-    $('#quote-count').text(`Quote ${quoteCount} of ${allStatements.length}`);
+    $('#quote-count').text(`Quote ${quoteCount} of ${allStatementsUrl.length}`);
     $('#submit').addClass('off');
     $('#prompt h2').text('Can you guess which politician said this?');
-    $('#context').text(`Said in ${statement.statementContext}.`);
+    $('#context').text(`Said in ${statement.statement.statementContext}.`);
     $('#truth-photo').addClass('off');
     $('#summary-header').addClass('off');
     $('#ruling-summary').addClass('off');
@@ -257,7 +263,6 @@
     $('#position').text('');
     $('#party').text('');
     $('#state').text('');
-
     stepOneComplete = false;
     stepTwoComplete = false;
     $('.p-choices').toggleClass('off');
@@ -266,7 +271,7 @@
   const buildResults = function() {
     let countQuotes = 1;
 
-    for (const statement of statementsObjSet) {
+    for (const statement of allStatementsData) {
       const $ul = $('.results-list');
       const $li = $('<li>');
       const $divI = $('<div>').addClass('collapsible-header');
@@ -278,9 +283,9 @@
       const $divHead = $('<div>').addClass('results');
       const $photo = $('<img>').attr('src', statement.speaker.photoUrl);
       const $speaker = $('<p>').text(`${statement.speaker.name} is a  ${statement.speaker.party} and ${statement.speaker.position}.`);
-      const $quote = $('<p>').text('Statement: ' + statement.quote);
-      const $yourGuesses = $('<p>').text(`You thought ${statement.speakerGuess} said this and guessed that it was ${statement.truthGuess}.`);
-      const $truth = $('<p>').text(`This statement was ${statement.ruling}`);
+      const $quote = $('<p>').text('Statement: ' + statement.statement.quote);
+      const $yourGuesses = $('<p>').text(`You thought ${statement.user.speakerGuess} said this and guessed that it was ${statement.user.truthGuess}.`);
+      const $truth = $('<p>').text(`This statement was ${statement.ruling.ruling}`);
 
       $li.append($divI);
       $divHead.append($photo);
@@ -366,6 +371,7 @@
     organizeData();
     setCount = 0;
     nextQuestion();
+    $('#submit').text('Next Question');
     $('#add-quotes').addClass('off');
   });
 })();
